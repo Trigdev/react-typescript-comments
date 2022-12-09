ee8a75b2ecc2e8c3b7cf84370b3f869252aacd78
Trigdev <ahorisaac.ai@gmail.com>
2022-12-09 14:58:34 +0100

    Project Development Completed, v1.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { FC, ReactElement, useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import data from "./data";
+import { Comment } from "./Comment";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: FC = (): ReactElement => {
+  const [comments, setComments] = useState(data);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <div className="container-fluid mt-auto">
+        <h2 className="h2 text-center my-4 text-light"> COMMENTIFY </h2>
+        <Comment comments={comments} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default App
+export default App;
